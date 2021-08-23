581a3bc9ed2533f826c6a07ed5e8fdbecf553634
root-marco <rootslowed@gmail.com>
2021-08-23 15:05:09 -0300

    subreddity model implemented

---
+++ src/main/java/com/slowed/reddity/model/Subreddity.java
@@ -0,0 +1,41 @@
+package com.slowed.reddity.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.Instant;
+import java.util.List;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Subreddity {
+
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private Long id;
+
+  @NotBlank(message = "community name is required")
+  private String name;
+
+  @NotBlank(message = "description is required")
+  private String description;
+
+  @OneToMany(fetch = LAZY)
+  private List<Post> posts;
+
+  private Instant createdDate;
+
+  @ManyToOne(fetch = LAZY)
+  private User user;
+
+}
