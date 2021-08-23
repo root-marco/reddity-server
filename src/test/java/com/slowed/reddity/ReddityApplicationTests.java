dfe4024e68b32d82104b9eef148785353c438ad5
root-marco <rootslowed@gmail.com>
2021-08-23 15:05:33 -0300

    reddity app tests

---
+++ src/test/java/com/slowed/reddity/ReddityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slowed.reddity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReddityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
