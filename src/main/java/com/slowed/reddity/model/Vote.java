a65e9d061b15665924f21b17f809b9e2b0d8c848
root-marco <rootslowed@gmail.com>
2021-08-23 15:31:31 -0300

    vote model implemented

---
+++ src/main/java/com/slowed/reddity/model/Vote.java
@@ -0,0 +1,36 @@
+package com.slowed.reddity.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Vote {
+
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private Long id;
+
+  private VoteType voteType;
+
+  @NotNull
+  @ManyToOne(fetch = LAZY)
+  @JoinColumn(name = "id", referencedColumnName = "id")
+  private Post post;
+
+  @ManyToOne(fetch = LAZY)
+  @JoinColumn(name = "id", referencedColumnName = "id")
+  private User user;
+
+}
