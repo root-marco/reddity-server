ca3d2a319b313398d6f2291e27a7b80a0d46d35d
root-marco <rootslowed@gmail.com>
2021-08-24 09:56:35 -0300

    reddity app

---
+++ src/main/java/com/slowed/reddity/ReddityApplication.java
@@ -0,0 +1,13 @@
+package com.slowed.reddity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReddityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReddityApplication.class, args);
+	}
+
+}
