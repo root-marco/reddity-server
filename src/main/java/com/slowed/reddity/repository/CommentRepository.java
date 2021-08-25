edb670318659aab121eca59383c12abc17b556a4
root-marco <rootslowed@gmail.com>
2021-08-25 10:04:38 -0300

    comment repository extends jpa repository

---
+++ src/main/java/com/slowed/reddity/repository/CommentRepository.java
@@ -0,0 +1,10 @@
+package com.slowed.reddity.repository;
+
+import com.slowed.reddity.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+
+
+}
