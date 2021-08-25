ca7982ba386ced8b51573ad8de0334ff98b8a236
root-marco <rootslowed@gmail.com>
2021-08-25 09:59:30 -0300

    post repository extends jpa repository

---
+++ src/main/java/com/slowed/reddity/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.slowed.reddity.repository;
+
+import com.slowed.reddity.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+
+
+}
