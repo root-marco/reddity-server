992c65478c515570a862f427b5029dfdd52470cd
root-marco <rootslowed@gmail.com>
2021-08-25 09:59:40 -0300

    subreddity repository extends jpa repository

---
+++ src/main/java/com/slowed/reddity/repository/SubreddityRepository.java
@@ -0,0 +1,10 @@
+package com.slowed.reddity.repository;
+
+import com.slowed.reddity.model.Subreddity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubreddityRepository extends JpaRepository<Subreddity, Long> {
+
+
+
+}
