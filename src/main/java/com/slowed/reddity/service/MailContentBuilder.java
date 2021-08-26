853386a4ce32bf09bccc302dae3d97e15f4ed42d
root-marco <rootslowed@gmail.com>
2021-08-26 10:27:52 -0300

    adding some spaces

--- src/main/java/com/slowed/reddity/service/MailContentBuilder.java
+++ src/main/java/com/slowed/reddity/service/MailContentBuilder.java
@@ -12,9 +12,11 @@ public class MailContentBuilder {
   private final TemplateEngine templateEngine;
 
   public String build(String message) {
+
     Context context = new Context();
     context.setVariable("message", message);
     return templateEngine.process("mailTemplate", context);
+
   }
 
 }
