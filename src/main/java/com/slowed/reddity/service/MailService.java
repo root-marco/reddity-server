07411ca30381bca34f029bb0e9d0156bf14e77cc
root-marco <rootslowed@gmail.com>
2021-08-26 11:49:21 -0300

    logic to send the email to the user

--- src/main/java/com/slowed/reddity/service/MailService.java
+++ src/main/java/com/slowed/reddity/service/MailService.java
@@ -8,6 +8,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,8 +17,8 @@ import org.springframework.stereotype.Service;
 public class MailService {
 
   private final JavaMailSender mailSender;
-  private final MailContentBuilder mailContentBuilder;
 
+  @Async
   public void sendMail(NotificationEmail notificationEmail) {
 
     MimeMessagePreparator messagePreparator = mimeMessage -> {
@@ -25,14 +26,15 @@ public class MailService {
       messageHelper.setFrom("reddity@email.com");
       messageHelper.setTo(notificationEmail.getRecipient());
       messageHelper.setSubject(notificationEmail.getSubject());
-      messageHelper.setText(mailContentBuilder.build(notificationEmail.getBody()));
+      messageHelper.setText(notificationEmail.getBody());
     };
 
     try {
       mailSender.send(messagePreparator);
       log.info("activation email sent!");
-    } catch (MailException ex) {
-      throw new SpringRedditException("exception occurred when sending email", ex);
+    } catch (MailException e) {
+      log.error("exception occurred when sending mail", e);
+      throw new SpringRedditException("exception occurred when sending mail to " + notificationEmail.getRecipient(), e);
     }
 
   }
