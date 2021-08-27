aa0efbde8049d4e10fed0fee5868d49b6b1ea14a
root-marco <rootslowed@gmail.com>
2021-08-27 09:37:43 -0300

    method to find by username

--- src/main/java/com/slowed/reddity/repository/UserRepository.java
+++ src/main/java/com/slowed/reddity/repository/UserRepository.java
@@ -3,8 +3,10 @@ package com.slowed.reddity.repository;
 import com.slowed.reddity.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-
+  Optional<User> findByUsername(String username);
 
 }
