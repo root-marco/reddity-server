4cf6d700ad94d9deea40343d5432ab21ad409979
root-marco <rootslowed@gmail.com>
2021-08-27 09:37:52 -0300

    method to find by token

--- src/main/java/com/slowed/reddity/repository/VerificationTokenRepository.java
+++ src/main/java/com/slowed/reddity/repository/VerificationTokenRepository.java
@@ -3,8 +3,10 @@ package com.slowed.reddity.repository;
 import com.slowed.reddity.model.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
 
-
+  Optional<VerificationToken> findByToken(String token);
 
 }
