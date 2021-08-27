d2206ef9846922589c8db49ac318eb78712e9578
root-marco <rootslowed@gmail.com>
2021-08-27 09:37:16 -0300

    async enabled

--- src/main/java/com/slowed/reddity/ReddityApplication.java
+++ src/main/java/com/slowed/reddity/ReddityApplication.java
@@ -2,8 +2,10 @@ package com.slowed.reddity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ReddityApplication {
 
 	public static void main(String[] args) {
