9b22be4d3344eb15d5ebb39fb0a315d395ac2fa9
root-marco <rootslowed@gmail.com>
2021-08-28 13:00:02 -0300

    jwt provider

---
+++ src/main/java/com/slowed/reddity/security/JwtProvider.java
@@ -0,0 +1,54 @@
+package com.slowed.reddity.security;
+
+import com.slowed.reddity.exceptions.SpringReddityException;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.*;
+import java.security.cert.CertificateException;
+
+@Service
+public class JwtProvider {
+
+  private KeyStore keyStore;
+
+  @PostConstruct
+  public void init() {
+
+    try {
+      keyStore = KeyStore.getInstance("JKS");
+      InputStream resourceAsStream = getClass().getResourceAsStream("/reddity.jks");
+      keyStore.load(resourceAsStream, "secret".toCharArray());
+    } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
+      throw new SpringReddityException("exception occurred while loading keystore");
+    }
+
+  }
+
+  public String generateToken(Authentication authentication) {
+
+    org.springframework.security.core.userdetails.User principal = (User) authentication.getPrincipal();
+
+    return Jwts.builder()
+      .setSubject(principal.getUsername())
+      .signWith(getPrivateKey())
+      .compact();
+
+  }
+
+  private PrivateKey getPrivateKey() {
+
+    try {
+      return (PrivateKey) keyStore.getKey("springblog", "secret".toCharArray());
+    } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
+      throw new SpringReddityException("exception occurred while retrieving public key from keystore");
+    }
+
+  }
+
+}
