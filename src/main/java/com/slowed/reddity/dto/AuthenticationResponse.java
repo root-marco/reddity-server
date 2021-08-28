4517e576e1a8bfd85dcaa067ef1fd3a2d952eb16
root-marco <rootslowed@gmail.com>
2021-08-28 12:58:55 -0300

    DTO AuthenticationResponse with token and username

---
+++ src/main/java/com/slowed/reddity/dto/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.slowed.reddity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthenticationResponse {
+
+  private String authenticationToken;
+
+  private String username;
+
+}
