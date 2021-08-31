59e0fdaa7fac6f3c1c24e0fb664a7f57f7359c24
root-marco <rootslowed@gmail.com>
2021-08-30 23:05:35 -0300

    filter before added JwtAuthenticationFilter

--- src/main/java/com/slowed/reddity/config/SecurityConfig.java
+++ src/main/java/com/slowed/reddity/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.slowed.reddity.config;
 
+import com.slowed.reddity.security.JwtAuthenticationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -12,12 +13,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.Filter;
 
 @EnableWebSecurity
 @AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final UserDetailsService userDetailsService;
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
   @Bean(BeanIds.AUTHENTICATION_MANAGER)
   @Override
@@ -30,14 +35,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   public void configure(HttpSecurity httpSecurity) throws Exception {
 
-    httpSecurity
-      .csrf()
-      .disable()
+    httpSecurity.csrf().disable()
       .authorizeRequests()
       .antMatchers("/api/auth/**")
       .permitAll()
       .anyRequest()
       .authenticated();
+
+    httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
   }
 
