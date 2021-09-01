e771aa3039c4c4ee341315b5a868d06373e9f61d
rootslowed <rootslowed@gmail.com>
2021-09-01 16:04:59 -0300

    subreddity not found exception

---
+++ src/main/java/com/slowed/reddity/exceptions/SubreddityNotFoundException.java
@@ -0,0 +1,11 @@
+package com.slowed.reddity.exceptions;
+
+public class SubreddityNotFoundException extends RuntimeException {
+
+  public SubreddityNotFoundException(String message) {
+
+    super(message);
+
+  }
+
+}
