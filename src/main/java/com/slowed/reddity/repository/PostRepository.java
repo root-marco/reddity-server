9f7d11de40937f52436dbf2b51e7bb7d2f7ea15b
rootslowed <rootslowed@gmail.com>
2021-09-01 17:33:47 -0300

    findAllBySubreddity and findByUser methods

--- src/main/java/com/slowed/reddity/repository/PostRepository.java
+++ src/main/java/com/slowed/reddity/repository/PostRepository.java
@@ -1,10 +1,16 @@
 package com.slowed.reddity.repository;
 
 import com.slowed.reddity.model.Post;
+import com.slowed.reddity.model.Subreddity;
+import com.slowed.reddity.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+  List<Post> findAllBySubreddity(Subreddity subreddity);
 
+  List<Post> findByUser(User user);
 
 }
