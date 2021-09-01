531b9f4e0425a38f4664517bd98440506c3d31a1
rootslowed <rootslowed@gmail.com>
2021-09-01 15:13:44 -0300

    subreddity service refactored

--- src/main/java/com/slowed/reddity/service/SubreddityService.java
+++ src/main/java/com/slowed/reddity/service/SubreddityService.java
@@ -2,6 +2,7 @@ package com.slowed.reddity.service;
 
 import com.slowed.reddity.dto.SubreddityDTO;
 import com.slowed.reddity.exceptions.SpringReddityException;
+import com.slowed.reddity.mapper.SubreddityMapper;
 import com.slowed.reddity.model.Subreddity;
 import com.slowed.reddity.repository.SubreddityRepository;
 import lombok.AllArgsConstructor;
@@ -19,11 +20,12 @@ import static java.util.stream.Collectors.toList;
 public class SubreddityService {
 
   private final SubreddityRepository subreddityRepository;
+  private final SubreddityMapper subreddityMapper;
 
   @Transactional
   public SubreddityDTO save(SubreddityDTO subreddityDTO) {
 
-    Subreddity save = subreddityRepository.save(mapToSubreddit(subreddityDTO));
+    Subreddity save = subreddityRepository.save(subreddityMapper.mapDTOToSubreddity(subreddityDTO));
 
     subreddityDTO.setId(save.getSubreddityId());
 
@@ -37,7 +39,7 @@ public class SubreddityService {
     return subreddityRepository
       .findAll()
       .stream()
-      .map(this::mapToDto)
+      .map(subreddityMapper::mapSubreddityToDTO)
       .collect(toList());
 
   }
@@ -48,26 +50,7 @@ public class SubreddityService {
       .findById(id)
       .orElseThrow(() -> new SpringReddityException("no subreddity found with ID - " + id));
 
-    return mapToDto(subreddity);
-
-  }
-
-  private SubreddityDTO mapToDto(Subreddity subreddity) {
-
-    return SubreddityDTO.builder()
-      .name(subreddity.getName())
-      .id(subreddity.getSubreddityId())
-      .numberOfPosts(subreddity.getPosts().size())
-      .build();
-
-  }
-
-  private Subreddity mapToSubreddit(SubreddityDTO subreddityDTO) {
-
-    return Subreddity.builder()
-      .name("/r/" + subreddityDTO.getName())
-      .description(subreddityDTO.getDescription())
-      .build();
+    return subreddityMapper.mapSubreddityToDTO(subreddity);
 
   }
 
