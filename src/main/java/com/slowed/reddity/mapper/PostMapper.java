b167ad4a85859e016082edadf7224578dbd00a9b
rootslowed <rootslowed@gmail.com>
2021-09-01 17:33:32 -0300

    post mapper to convert DTO to MODEL and vice versa

---
+++ src/main/java/com/slowed/reddity/mapper/PostMapper.java
@@ -0,0 +1,28 @@
+package com.slowed.reddity.mapper;
+
+import com.slowed.reddity.dto.PostRequest;
+import com.slowed.reddity.dto.PostResponse;
+import com.slowed.reddity.model.Post;
+import com.slowed.reddity.model.Subreddity;
+import com.slowed.reddity.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface PostMapper {
+
+  @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+  @Mapping(target = "subreddity", source = "subreddity")
+  @Mapping(target = "user", source = "user")
+  @Mapping(target = "description", source = "postRequest.description")
+  Post map(PostRequest postRequest, Subreddity subreddity, User user);
+
+  @Mapping(target = "postId", source = "postId")
+  @Mapping(target = "postName", source = "postName")
+  @Mapping(target = "description", source = "description")
+  @Mapping(target = "url", source = "url")
+  @Mapping(target = "subreddityName", source = "subreddity.name")
+  @Mapping(target = "userName", source = "user.username")
+  PostResponse mapToDTO(Post post);
+
+}
