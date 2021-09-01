ab6c3474a68d1006a43e7401b299c48e2a3089de
rootslowed <rootslowed@gmail.com>
2021-09-01 17:34:20 -0300

    findByName subreddity repo

--- src/main/java/com/slowed/reddity/repository/SubreddityRepository.java
+++ src/main/java/com/slowed/reddity/repository/SubreddityRepository.java
@@ -3,8 +3,10 @@ package com.slowed.reddity.repository;
 import com.slowed.reddity.model.Subreddity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SubreddityRepository extends JpaRepository<Subreddity, Long> {
 
-
+  Optional<Subreddity> findByName(String subreddityName);
 
 }
