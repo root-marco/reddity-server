23c84434c162135289dabded71f4c65008a8c02d
rootslowed <rootslowed@gmail.com>
2021-09-04 11:44:17 -0300

    comment repository with findByPost and findAllByUser

--- src/main/java/com/slowed/reddity/repository/CommentRepository.java
+++ src/main/java/com/slowed/reddity/repository/CommentRepository.java
@@ -1,10 +1,16 @@
 package com.slowed.reddity.repository;
 
 import com.slowed.reddity.entity.Comment;
+import com.slowed.reddity.entity.Post;
+import com.slowed.reddity.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+  List<Comment> findByPost(Post post);
 
+  List<Comment> findAllByUser(User user);
 
 }
