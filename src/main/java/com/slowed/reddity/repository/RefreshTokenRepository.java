2516b86a59ebc455ae3054ae8b9876192e750ba3
rootslowed <rootslowed@gmail.com>
2021-09-09 11:50:22 -0300

    refresh token repository with find by token and delete by token

--- src/main/java/com/slowed/reddity/repository/RefreshTokenRepository.java
+++ src/main/java/com/slowed/reddity/repository/RefreshTokenRepository.java
@@ -3,8 +3,12 @@ package com.slowed.reddity.repository;
 import com.slowed.reddity.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
+  Optional<RefreshToken> findByToken(String token);
 
+  void deleteByToken(String token);
 
 }
