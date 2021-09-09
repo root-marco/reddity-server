032161cdfc1cec064380197581a1cf60393da6b5
rootslowed <rootslowed@gmail.com>
2021-09-09 11:50:07 -0300

    post mapper done

--- src/main/java/com/slowed/reddity/mapper/PostMapper.java
+++ src/main/java/com/slowed/reddity/mapper/PostMapper.java
@@ -1,28 +1,88 @@
 package com.slowed.reddity.mapper;
 
+import com.github.marlonlom.utilities.timeago.TimeAgo;
 import com.slowed.reddity.dto.PostRequest;
 import com.slowed.reddity.dto.PostResponse;
-import com.slowed.reddity.entity.Post;
-import com.slowed.reddity.entity.Subreddity;
-import com.slowed.reddity.entity.User;
+import com.slowed.reddity.entity.*;
+import com.slowed.reddity.repository.CommentRepository;
+import com.slowed.reddity.repository.VoteRepository;
+import com.slowed.reddity.service.AuthService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+import static com.slowed.reddity.entity.VoteType.UPVOTE;
+import static com.slowed.reddity.entity.VoteType.DOWNVOTE;
 
 @Mapper(componentModel = "spring")
-public interface PostMapper {
+public abstract class PostMapper {
 
-  @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
-  @Mapping(target = "subreddity", source = "subreddity")
+  @Autowired
+  private CommentRepository commentRepository;
+  @Autowired
+  private VoteRepository voteRepository;
+  @Autowired
+  private AuthService authService;
+
   @Mapping(target = "user", source = "user")
+  @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
   @Mapping(target = "description", source = "postRequest.description")
-  Post map(PostRequest postRequest, Subreddity subreddity, User user);
+  @Mapping(target = "subreddity", source = "subreddity")
+  @Mapping(target = "voteCount", constant = "0")
+  public abstract Post map(PostRequest postRequest, Subreddity subreddity, User user);
 
   @Mapping(target = "postId", source = "postId")
-  @Mapping(target = "postName", source = "postName")
-  @Mapping(target = "description", source = "description")
-  @Mapping(target = "url", source = "url")
   @Mapping(target = "subreddityName", source = "subreddity.name")
   @Mapping(target = "userName", source = "user.username")
-  PostResponse mapToDTO(Post post);
+  @Mapping(target = "commentCount", expression = "java(commentCount(post))")
+  @Mapping(target = "duration", expression = "java(getDuration(post))")
+  @Mapping(target = "upVote", expression = "java(isPostUpVoted(post))")
+  @Mapping(target = "downVote", expression = "java(isPostDownVoted(post))")
+  public abstract PostResponse mapToDTO(Post post);
+
+  Integer commentCount(Post post) {
+
+    return commentRepository
+      .findByPost(post).size();
+
+  }
+
+  String getDuration(Post post) {
+
+    return TimeAgo
+      .using(post.getCreatedDate()
+      .toEpochMilli());
+
+  }
+
+  boolean isPostUpVoted(Post post) {
+
+    return checkVoteType(post, UPVOTE);
+
+  }
+
+  boolean isPostDownVoted(Post post) {
+
+    return checkVoteType(post, DOWNVOTE);
+
+  }
+
+  private boolean checkVoteType(Post post, VoteType voteType) {
+
+    if (authService.isLoggedIn()) {
+
+      Optional<Vote> voteForPostByUser = voteRepository
+        .findTopByPostAndUserOrderByVoteIdDesc(post, authService.getCurrentUser());
+
+      return voteForPostByUser.filter(vote ->
+        vote.getVoteType().equals(voteType)).isPresent();
+
+    }
+
+    return false;
+
+  }
 
 }
