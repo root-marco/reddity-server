8c52338bd9478fc19c81abcfa524a5f63f31e1e0
rootslowed <rootslowed@gmail.com>
2021-09-09 11:48:10 -0300

    auth controller with refresh token and logout mapping

--- src/main/java/com/slowed/reddity/controller/AuthController.java
+++ src/main/java/com/slowed/reddity/controller/AuthController.java
@@ -2,12 +2,16 @@ package com.slowed.reddity.controller;
 
 import com.slowed.reddity.dto.AuthenticationResponse;
 import com.slowed.reddity.dto.LoginRequest;
+import com.slowed.reddity.dto.RefreshTokenRequest;
 import com.slowed.reddity.dto.RegisterRequest;
 import com.slowed.reddity.service.AuthService;
+import com.slowed.reddity.service.RefreshTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
   private final AuthService authService;
+  private final RefreshTokenService refreshTokenService;
 
   @PostMapping("/signup")
   public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
@@ -36,6 +41,23 @@ public class AuthController {
 
     authService.verifyAccount(token);
     return new ResponseEntity<>("account activated successfully", HttpStatus.OK);
+
+  }
+
+  @PostMapping("/refresh/token")
+  public AuthenticationResponse refreshTokens(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+
+    return authService.refreshToken(refreshTokenRequest);
+
+  }
+
+  @PostMapping("/logout")
+  public ResponseEntity<String> logout(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+
+    refreshTokenService.deleteRefreshToken(refreshTokenRequest.getRefreshToken());
+
+    return ResponseEntity.status(HttpStatus.OK)
+      .body("refresh token deleted successfully!");
 
   }
 
