1d7a767bfb8ae48a8169d749d6320c570f06b2ff
rootslowed <rootslowed@gmail.com>
2021-09-09 11:52:01 -0300

    find by post and user order by vote id description

--- src/main/java/com/slowed/reddity/repository/VoteRepository.java
+++ src/main/java/com/slowed/reddity/repository/VoteRepository.java
@@ -1,10 +1,14 @@
 package com.slowed.reddity.repository;
 
+import com.slowed.reddity.entity.Post;
+import com.slowed.reddity.entity.User;
 import com.slowed.reddity.entity.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VoteRepository extends JpaRepository<Vote, Long> {
 
-
+  Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, User currentUser);
 
 }
